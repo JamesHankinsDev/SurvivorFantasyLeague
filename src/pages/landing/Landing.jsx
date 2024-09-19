4d4c2430f3e1b52e8f6cd25a7eba3c1f86d0ad88
JHankins09 <Hankins.JamesB@gmail.com>
2024-09-18 20:12:34 -0400

    init commit

---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+
+const Landing = () => {
+  return (
+    <div className="columns-2 h-screen">
+      <div className="bg-black h-full">
+        <h1 className="text-3xl">Landing Page</h1>
+      </div>
+      <div className="bg-white h-full">
+        <h2 className="text-2xl">Second Part</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
