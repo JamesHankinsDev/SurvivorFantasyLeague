d34fec44f26995fa965f28819b1df25b41bcd8dd
JHankins09 <Hankins.JamesB@gmail.com>
2024-09-24 20:43:42 -0400

    redirect to Prod API

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,12 +2,12 @@ export const getAPIURI = () => {
   const currentUri = window.location.href;
 
   console.log(
-    !currentUri.includes('localhost')
+    currentUri.includes('localhost')
       ? `http://localhost:5000`
       : `https://survivorfantasyserver.onrender.com`
   );
 
-  return !currentUri.includes('localhost')
+  return currentUri.includes('localhost')
     ? `http://localhost:5000`
     : `https://survivorfantasyserver.onrender.com`;
 };
