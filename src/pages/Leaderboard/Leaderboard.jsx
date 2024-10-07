120bf6887ecfeb3dae61a0ecae74dce925177e2b
JHankins09 <Hankins.JamesB@gmail.com>
2024-10-07 14:26:28 -0400

    mobile-friendly leaderboard

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -32,6 +32,10 @@ const Leaderboard = () => {
       <div className={'flex flex-row justify-between items-center'}>
         <div className={'text-xl font-bold text-slate-900'}>
           Welcome to the Leaderboard
+          <span className={'md:hidden text-sm'}>
+            <br />
+            See team castaways on Desktop!
+          </span>
         </div>
         {showAllTribes ? (
           <button
@@ -42,7 +46,7 @@ const Leaderboard = () => {
           </button>
         ) : (
           <button
-            className={'boton-elegante'}
+            className={'boton-elegante md:block hidden'}
             onClick={() => setShowAllTribes(true)}
           >
             Show All Tribes
@@ -57,12 +61,20 @@ const Leaderboard = () => {
             className={`flex justify-center items-center w-full`}
           >
             <div
-              className={'flex flex-col bg-slate-600 p-5 my-2 rounded w-9/12'}
+              className={
+                'flex flex-col bg-slate-600 md:p-5 my-2 rounded md:w-9/12 w-full'
+              }
             >
-              <div className={'flex flex-row justify-between items-center m-2'}>
-                <div className={'text-md text-slate-300'}>
+              <div
+                className={
+                  'flex md:flex-row flex-col justify-between items-center m-2'
+                }
+              >
+                <div className={'md:text-md text-sm text-slate-300'}>
                   Current Rank:
-                  <div className={'text-5xl font-extrabold text-center'}>
+                  <div
+                    className={'md:text-5xl text-lg font-extrabold text-center'}
+                  >
                     {allTribes.indexOf(t) + 1}
                     {allTribes.indexOf(t) + 1 === 1
                       ? 'st'
@@ -75,56 +87,60 @@ const Leaderboard = () => {
                 </div>
                 <div
                   className={
-                    'text-3xl font-bold bg-slate-300 text-slate-700 p-2 rounded'
+                    'md:text-3xl text-lg font-bold bg-slate-300 text-slate-700 p-2 rounded'
                   }
                 >
                   {t.userId.username}
                 </div>
-                <div className={'text-md text-slate-300'}>
+                <div className={'md:text-md text-sm md:p-0 p-2 text-slate-300'}>
                   Total Points:
-                  <div className={'text-5xl font-extrabold text-center'}>
+                  <div
+                    className={'md:text-5xl text-md font-extrabold text-center'}
+                  >
                     {t.totalPoints}
                   </div>
                 </div>
               </div>
-              {showTribe === t._id || showAllTribes ? (
-                <>
-                  <div className={'flex justify-center align-center mb-2'}>
+              <div className={'md:block hidden'}>
+                {showTribe === t._id || showAllTribes ? (
+                  <>
+                    <div className={'flex justify-center align-center mb-2'}>
+                      <button
+                        className={'boton-elegante'}
+                        onClick={() => setShowTribe(null)}
+                      >
+                        Hide Tribe Members
+                      </button>
+                    </div>
+                    <div
+                      className={
+                        'text-md text-slate-300 text-center italic text-sm'
+                      }
+                    >
+                      Current Tribe:
+                    </div>
+                    <hr className={'border-slate-300 m-3'} />
+                    <div className={'grid grid-cols-5 gap-3'}>
+                      {t.castaways.map((c) => (
+                        <CastawayCard
+                          key={`leaderboard__castaways__${c._id}`}
+                          castaway={c}
+                          handleClick={null}
+                        />
+                      ))}
+                    </div>
+                  </>
+                ) : (
+                  <div className={'flex justify-center align-center'}>
                     <button
                       className={'boton-elegante'}
-                      onClick={() => setShowTribe(null)}
+                      onClick={() => setShowTribe(t._id)}
                     >
-                      Hide Tribe Members
+                      Show Tribe Members
                     </button>
                   </div>
-                  <div
-                    className={
-                      'text-md text-slate-300 text-center italic text-sm'
-                    }
-                  >
-                    Current Tribe:
-                  </div>
-                  <hr className={'border-slate-300 m-3'} />
-                  <div className={'grid grid-cols-5 gap-3'}>
-                    {t.castaways.map((c) => (
-                      <CastawayCard
-                        key={`leaderboard__castaways__${c._id}`}
-                        castaway={c}
-                        handleClick={null}
-                      />
-                    ))}
-                  </div>
-                </>
-              ) : (
-                <div className={'flex justify-center align-center'}>
-                  <button
-                    className={'boton-elegante'}
-                    onClick={() => setShowTribe(t._id)}
-                  >
-                    Show Tribe Members
-                  </button>
-                </div>
-              )}
+                )}
+              </div>
             </div>
           </div>
         ))}
